395fed88b26348cd7e02069c208f8afd5235e235
Steven Lambion <steven.lambion@atsid.com>
2019-06-13 19:41:30 -0700

    Fix indentation

--- Package.swift
+++ Package.swift
@@ -4,29 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftDux",
-    platforms: [
-      .iOS(.v13),
-      .macOS(.v10_15),
+  name: "SwiftDux",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
   ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftDux",
-            targets: ["SwiftDux"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftDux",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftDuxTests",
-            dependencies: ["SwiftDux"]),
-    ]
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "SwiftDux",
+      targets: ["SwiftDux"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "SwiftDux",
+      dependencies: []),
+    .testTarget(
+      name: "SwiftDuxTests",
+      dependencies: ["SwiftDux"]),
+  ]
 )
