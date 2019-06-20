9d8e5992cf06dfe5b2638c695e30f5e92c248c08
Steven Lambion <steven.lambion@atsid.com>
2019-06-19 23:45:09 -0700

    updated docs

--- Guides/Installation.md
+++ Guides/Installation.md
@@ -13,7 +13,7 @@ import PackageDescription
 
 let package = Package(
   dependencies: [
-    .Package(url: "https://github.com/StevenLambion/SwiftDux.git", majorVersion: 0, minor: 4)
+    .Package(url: "https://github.com/StevenLambion/SwiftDux.git", majorVersion: 0, minor: 5)
   ]
 )
 ```
--- Sources/SwiftDux/UI/MapState.swift
+++ Sources/SwiftDux/UI/MapState.swift
@@ -11,22 +11,27 @@ import SwiftUI
 /// ```
 @propertyDelegate
 public struct MapState<State: StateType> : DynamicViewProperty {
-  
+
   @EnvironmentObject private var storeContext: StoreContext<State>
-  
+
   public var value: State {
     nonmutating get {
       storeContext.store.state
     }
   }
-  
+
   public init() {}
-  
+
+  /// Create a binding for a mapped state value and an action.
+  /// - Parameters:
+  ///   - mapState: A mapper that returns the value from the current state.
+  ///   - update: A closure that maps the new value to an action to dispatch the change.
+  /// - Returns: A new `Binding<_>` of the state value.
   public func bind<T>(_ mapState: @escaping (State) -> T, update: @escaping (T) -> Action) -> Binding<T> {
     return Binding<T>(
       getValue: { mapState(self.value) } ,
       setValue: { self.storeContext.dispatcher.send(update($0)) }
     )
   }
-  
+
 }
