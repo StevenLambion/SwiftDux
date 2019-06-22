cc16a282aa6ed1031205df893a019992a34265d9
Steven Lambion <steven.lambion@atsid.com>
2019-06-21 18:52:29 -0700

    Added onAppear / onDisppear async methods to VIews

---
+++ Sources/SwiftDux/UI/View+AppearanceEvents.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+import Dispatch
+
+extension View {
+  
+  /// Performs an action asynchronously on the main thread when a view appears.
+  ///
+  /// SwiftUI doesn't update a view if changes are performed in the onAppear method.
+  /// This includes changes to the application state from dispatched actions. This method
+  /// allows actions to be dispatched when the view appears.
+  /// - Parameter perform: The action to run asynchronously
+  func onAppearAsync(perform: @escaping () -> ()) -> some View {
+    return self.onAppear {
+      DispatchQueue.main.async(execute: perform)
+    }
+  }
+  
+  /// Performs an action asynchronously on the main thread when a view appears.
+  /// - Parameter perform: The action to run asynchronously
+  func onDisappearAsync(perform: @escaping () -> ()) -> some View {
+    return self.onDisappear() {
+      DispatchQueue.main.async(execute: perform)
+    }
+  }
+  
+}
