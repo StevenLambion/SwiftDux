3323f7fa1180d98eb69aff3c4bb02f6d6736bdbc
Steven Lambion <steven.lambion@atsid.com>
2019-08-29 18:12:18 -0700

    Save on state change or when entering background (for release).

--- Sources/SwiftDuxExtras/Persistence/PersistStateMiddleware.swift
+++ Sources/SwiftDuxExtras/Persistence/PersistStateMiddleware.swift
@@ -1,23 +1,32 @@
 import Foundation
 import Combine
 import SwiftDux
+import UIKit
 
 /// Hooks up state peristence to the store.
-/// - Parameter persistor: The state persistor to use.
+/// - Parameters
+///   - persistor: The state persistor to use.
+///   - saveOnChange: Saves the state when it changes, else, it saves when the app enters the backgroound.
+///   - debounceFor: The debounce interval for saving on changes.
 public func PersistStateMiddleware<State, SP> (
-  _ persistor: SP
+  _ persistor: SP,
+  saveOnChange: Bool = true,
+  debounceFor interval: RunLoop.SchedulerTimeType.Stride = .milliseconds(100)
 ) -> Middleware<State> where SP : StatePersistor, State == SP.State, SP.Failure == Never, SP.Input == State {
   { store in { action in
-    switch action as? StoreAction {
-    case .prepare:
-      persistor.save(from: store)
-      if let state = persistor.restore() {
-        store.send(PersistStateAction<State>.restore(state: state))
-      }
-    default:
-      break
+    defer { store.next(action) }
+    guard case .prepare = action as? StoreAction else { return }
+    if saveOnChange {
+      persistor.save(from: store, debounceFor: interval)
+    } else {
+      let publisher = NotificationCenter.default
+        .publisher(for: UIApplication.didEnterBackgroundNotification)
+        .compactMap { _ in store.state }
+      persistor.save(from: publisher)
     }
-    store.next(action)
+    if let state = persistor.restore() {
+      store.send(PersistStateAction<State>.restore(state: state))
+    }
   }}
 }
 
