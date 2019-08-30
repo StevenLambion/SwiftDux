7118cf74ce63488d5826e33a3815891ea94810ab
Steven Lambion <steven.lambion@atsid.com>
2019-08-29 20:48:56 -0700

    Added parameter to validate state before restoring.

--- Sources/SwiftDuxExtras/Persistence/PersistStateMiddleware.swift
+++ Sources/SwiftDuxExtras/Persistence/PersistStateMiddleware.swift
@@ -8,14 +8,17 @@ import UIKit
 ///   - persistor: The state persistor to use.
 ///   - saveOnChange: Saves the state when it changes, else, it saves when the app enters the backgroound.
 ///   - debounceFor: The debounce interval for saving on changes.
+///   - shouldRestore: Closure used to validate the state before restoring it. This is useful if the state's schema version has changed.
 public func PersistStateMiddleware<State, SP> (
   _ persistor: SP,
   saveOnChange: Bool = true,
-  debounceFor interval: RunLoop.SchedulerTimeType.Stride = .milliseconds(100)
+  debounceFor interval: RunLoop.SchedulerTimeType.Stride = .milliseconds(100),
+  shouldRestore: @escaping (State)->Bool = { _ in true }
 ) -> Middleware<State> where SP : StatePersistor, State == SP.State, SP.Failure == Never, SP.Input == State {
   { store in { action in
     defer { store.next(action) }
     guard case .prepare = action as? StoreAction<State> else { return }
+    
     if saveOnChange {
       persistor.save(from: store, debounceFor: interval)
     } else {
@@ -24,7 +27,8 @@ public func PersistStateMiddleware<State, SP> (
         .compactMap { _ in store.state }
       persistor.save(from: publisher)
     }
-    if let state = persistor.restore() {
+    
+    if let state = persistor.restore(), shouldRestore(state) {
       store.send(StoreAction<State>.reset(state: state))
     }
   }}
