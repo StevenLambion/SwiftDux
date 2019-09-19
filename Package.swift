f6a61678962f3b2236f2129aaa8e9e28faaffb43
Steven Lambion <steven.lambion@atsid.com>
2019-09-19 15:34:16 -0700

    Added swift-format

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
       targets: ["SwiftDux", "SwiftDuxExtras"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-format.git", .branch("master")),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
