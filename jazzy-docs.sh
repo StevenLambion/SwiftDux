3213de8deaef5e5e52e0cc84d67164ec37dd6a5d
Steven Lambion <steven.lambion@atsid.com>
2019-10-03 08:03:39 -0700

    Moved docs to gh-pages branch.

--- jazzy-docs.sh
+++ jazzy-docs.sh
@@ -4,6 +4,20 @@ if [ ! -e SwiftDux.xcodeproj ]; then
   swift package generate-xcodeproj
 fi
 
+# Set gh-pages branch to the docs directory.
+git worktree add docs gh-pages
+
+# Generate documentation
 jazzy -x USE_SWIFT_RESPONSE_FILE=NO
 mkdir -p ./docs/Guides/Images
 cp ./Guides/Images/* ./docs/Guides/Images/
+
+# Deploy to gh-pages branch locally.
+cd ./docs || exit
+
+git add --all
+git commit -m "Documentation changes"
+
+cd ..
+git worktree remove docs
+
