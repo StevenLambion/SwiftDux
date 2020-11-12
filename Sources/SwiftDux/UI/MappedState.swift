39463f6836428c238bdd73b1488b8da7aff70d54
Steven Lambion <steven.lambion@atsid.com>
2020-11-12 07:54:46 -0800

    Deprecated older SwiftDux APIs.

--- Sources/SwiftDux/UI/MappedState.swift
+++ Sources/SwiftDux/UI/MappedState.swift
@@ -2,14 +2,12 @@ import Foundation
 import SwiftUI
 
 /// Retrieves a mapping of the application state from the environment and provides it to a property in a SwiftUI view.
-/// Use the `connect(updateWhen:mapState:)` method to first inject the state from a parent view.
 /// ```
 /// struct MyView : View {
-///
 ///   @MappedState var todoList: TodoList
-///
 /// }
 /// ```
+@available(*, deprecated)
 @propertyWrapper
 public struct MappedState<State>: DynamicProperty {
   @Environment(\.store) private var anyStore
@@ -24,7 +22,6 @@ public struct MappedState<State>: DynamicProperty {
   }
 
   public mutating func update() {
-    guard store == nil else { return }
     self.store = anyStore.unwrap(as: State.self)
   }
 
